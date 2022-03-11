7d0b2683eda22444051cdbc0b681e3fc90151aa9
sdreyes <sheshechan90@gmail.com>
2022-03-11 15:44:31 -0500

    Add concurrently and nodemon dependencies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
