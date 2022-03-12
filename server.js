dc19c19dbfbd9895e37cedf57e467edd4061a56a
sdreyes <sheshechan90@gmail.com>
2022-03-12 10:07:14 -0500

    Restructure the directory and node package start scripts

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`🌎 ==> API server now on port ${PORT}!`));
 });
