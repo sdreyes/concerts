745880a69af69110f5fe2c2ac719881d966e47bd
sdreyes <sheshechan90@gmail.com>
2022-03-12 11:11:33 -0500

    Import the artist model so the table is created if it doesn't exist

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require("path");
+const Artist = require('./models/Artist');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
