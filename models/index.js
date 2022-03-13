f7e6534fd6fcfa04d49606b641b9fae5b1ce716b
sdreyes <sheshechan90@gmail.com>
2022-03-13 13:01:25 -0400

    Add many-to-many relationship between Attendee and Show

--- models/index.js
+++ models/index.js
@@ -5,6 +5,28 @@ const Attendee = require('./Attendee');
 const Lineup = require('./Lineup');
 const Audience = require('./Audience');
 
+Show.belongsToMany(Attendee, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Audience,
+    unique: false
+  },
+  foreignKey: 'showId',
+  // Define an alias for when data is retrieved
+  as: 'attendees'
+});
+
+Attendee.belongsToMany(Show, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Audience,
+    unique: false
+  },
+  foreignKey: 'attendeeId',
+  // Define an alias for when data is retrieved
+  as: 'shows'
+});
+
 module.exports = {
   Artist,
   Venue,
