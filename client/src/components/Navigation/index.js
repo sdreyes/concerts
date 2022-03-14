42a6a139d7bb713f67cb844df21bf9e12ae8a0ba
sdreyes <sheshechan90@gmail.com>
2022-03-13 23:02:06 -0400

    Remove link from navbar

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -5,7 +5,7 @@ function Navigation() {
   return (
     <Navbar bg="info" variant="light">
       <Container>
-        <Navbar.Brand href="#home">Samby's Shows</Navbar.Brand>
+        <Navbar.Brand>Samby's Shows</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/addshow">Add Show</Nav.Link>
