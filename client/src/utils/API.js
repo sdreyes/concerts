7d97a77cf81262ae42ef9c7f615e754d674cf81b
sdreyes <sheshechan90@gmail.com>
2022-03-16 21:25:24 -0400

    Add getVenues and getArtists API call functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,14 @@ const API = {
   getShows: function () {
     console.log("Getting shows...");
     return axios.get("/api/shows");
+  },
+  getArtists: function() {
+    console.log("Getting artists...");
+    return axios.get("/api/artists");
+  },
+  getVenues: function() {
+    console.log("Getting venues...");
+    return axios.get("/api/venues");
   }
 }
 
