e6b224ace238d4951f224f59196a06e3a2879fb1
sdreyes <sheshechan90@gmail.com>
2022-03-16 21:39:39 -0400

    Add to 'Add Show' page with additional components

---
+++ client/src/components/Forms/AddShowForm/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Form, Button, InputGroup, Col, Row } from 'react-bootstrap';
+
+export function AddShowForm() {
+  return (
+    <Form>
+      Test
+    </Form>
+  );
+}
--- client/src/pages/AddShow.js
+++ client/src/pages/AddShow.js
@@ -1,14 +1,47 @@
 import React, { Component, Fragment } from "react";
+import API from "../utils/API";
+import {Container, Row, Col} from 'react-bootstrap';
+import {AddShowForm} from '../components/Forms/AddShowForm';
 
 class AddShow extends Component {
   state = {
-    page: "show"
+    page: "show",
+    artists: [],
+    venues: []
   }
+  // Component Mounted
+  componentDidMount() {
+    this.loadArtists();
+    this.loadVenues();
+  }
+  // Load Artists
+  loadArtists = () => {
+    API.getArtists().then(
+      res => {
+        console.log(res.data);
+        this.setState({artists: res.data});
+      }
+    )
+  }
+  // Load Venues
+  loadVenues = () => {
+    API.getVenues().then(
+      res => {
+        console.log(res.data);
+        this.setState({venues: res.data});
+      }
+    )
+  }
+  // Render
   render() {
     return (
-      <div>
-        <h1>Add Show</h1>
-      </div>
+      <Container className="p-3">
+        <Row>
+          <Col>
+          <AddShowForm />
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
