ead5729bca31eb1e6d6f7873c664c2e74691609a
sdreyes <sheshechan90@gmail.com>
2022-03-27 19:07:27 -0400

    change post create to post bulkCreate

--- routes/api/lineupRoutes.js
+++ routes/api/lineupRoutes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 if (process.env.NODE_ENV !== "production") {
   router.post('/', async (req, res) => {
     try {
-      const lineupData = await Lineup.create(req.body);
+      const lineupData = await Lineup.bulkCreate(req.body);
       res.status(200).json(lineupData);
     } catch (err) {
       res.status(400).json(err);
