53eea5dd1f998d1594678e12f6043cf0cfa1395a
sdreyes <sheshechan90@gmail.com>
2022-03-27 19:07:44 -0400

    Change post create to post bulkCreate

--- routes/api/audienceRoutes.js
+++ routes/api/audienceRoutes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 if (process.env.NODE_ENV !== "production") {
   router.post('/', async (req, res) => {
     try {
-      const audienceData = await Audience.create(req.body);
+      const audienceData = await Audience.bulkCreate(req.body);
       res.status(200).json(audienceData);
     } catch (err) {
       res.status(400).json(err);
