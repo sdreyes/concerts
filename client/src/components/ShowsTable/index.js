c7408a021fba7db6ad1e0098188cdbe3ec9bfa98
sdreyes <sheshechan90@gmail.com>
2023-05-11 22:28:56 -0400

    Separate tables by year and remove the YYYY from the date column to reduce the width of each table for mobile friendliness

--- client/src/components/ShowsTable/index.js
+++ client/src/components/ShowsTable/index.js
@@ -1,36 +1,51 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { Table } from 'react-bootstrap';
 import moment from 'moment';
 
 export function ShowsTable({shows}) {
+  let showYears = [];
+  shows.forEach(show => {
+    if(!showYears.includes(moment(show.startDate).year())) {
+      showYears.push(moment(show.startDate).year())
+    }
+  });
   return(
-    <Table striped bordered>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Show</th>
-          <th>Venue</th>
-          <th>City</th>
-        </tr>
-      </thead>
-      <tbody>
-        {shows.map(show => {
+    <Fragment>
+      {showYears.map(year => {
           return (
-            <tr key={show.showId}>
-              <td>{moment(show.startDate).format("MM/DD/YYYY")}</td>
-              <td>
-                <a className="link-dark" href={`/show/${show.showId}`}>
-                  {show.title}
-                </a>
-              </td>
-              <td>{show.location.venue}</td>
-              <td>
-                {`${show.location.city}, ${show.location.state ? show.location.state : show.location.country}`}
-              </td>
-            </tr>
+            <Fragment key={year}>
+              <h2 className="text-center">{year}</h2>
+              <Table striped bordered>
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Show</th>
+                    <th>Venue</th>
+                    <th>City</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {shows.filter(show => moment(show.startDate).year() === year).map(show => {
+                    return (
+                      <tr key={show.showId}>
+                        <td>{moment(show.startDate).format("MM/DD")}</td>
+                        <td>
+                          <a className="link-dark" href={`/show/${show.showId}`}>
+                            {show.title}
+                          </a>
+                        </td>
+                        <td>{show.location.venue}</td>
+                        <td>
+                          {`${show.location.city}, ${show.location.state ? show.location.state : show.location.country}`}
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </Table>
+            </Fragment>
           )
-        })}
-      </tbody>
-    </Table>
+      })}
+    </Fragment>
   )
 };
